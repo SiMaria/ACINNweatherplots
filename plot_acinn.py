802809e733bff43a61ec148dc91a434f2a6bf4de
DESKTOP-FRQT47F\Exi-Museum <birgit.bacher1988@gmail.com>
2019-10-26 14:50:50 +0200

    added statistics & some minor visual improvements

--- plot_acinn.py
+++ plot_acinn.py
@@ -10,8 +10,9 @@ from bokeh.tile_providers import get_provider, Vendors
 #from bokeh.models.widgets import DataTable, TableColumn, Slider, Dropdown
 from bokeh.layouts import layout, column, row, widgetbox, gridplot
 from bokeh.io import output_file, save
-from bokeh.models.widgets import Panel, Tabs
+from bokeh.models.widgets import Panel, Tabs, Div
 from bokeh.models.widgets import DateRangeSlider
+
 
 
 def get_width():
@@ -64,38 +65,66 @@ def set_font_sizes_axis(p):
     p.toolbar.active_scroll = p.select_one(WheelZoomTool)
     return p
 
+def get_stats(df):
+    '''
+    make datatable with statistics
+    '''
+    if 'rrcum' in df.columns:
+        df = df.drop(columns='rrcum')
+    # mean
+    df_mean = df.resample('1D').mean()
+    df_mean = df_mean.transpose()
+    df_mean.columns.name = ''
+    # min
+    df_min = df.resample('1D').min()
+    df_min = df_min.transpose()
+    df_min.columns.name = ''
+    # max
+    cum = df.groupby(pd.Grouper(freq='D'))
+    if 'so' in df.columns:
+        df['ssd_cum'] = cum['so'].cumsum()
+    if 'rr' in df.columns:
+        df['rr_cum'] = cum['rr'].cumsum()
+    df_max = df.resample('1D').max()
+    df_max = df_max.transpose()
+    df_max.columns.name = ''
+    # stats
+    stats = pd.concat([df_mean, df_min, df_max], keys=['mean', 'min', 'max'])
+    stats.columns = stats.columns.strftime('%Y-%m-%d')
+    return stats
+
 ##### Plot 1
 def upper_plot(df):
     p1_tools = 'box_zoom, pan, save, hover, reset, xwheel_zoom' # zoom bounds auto?
-    p1 = figure(width = fwidth, height = fhgt, x_axis_type="datetime", 
-                tools=p1_tools, 
+    p1 = figure(width = fwidth, height = fhgt, x_axis_type="datetime",
+                tools=p1_tools,
                 x_range=(pd.to_datetime(df.index[-1])-timedelta(days=1), pd.to_datetime(df.index[-1])));
-                
+
     p1 = set_font_sizes_axis(p1)
     hover_p1 = p1.select(dict(type=HoverTool))
-    hover_p1.tooltips = [("Timestamp", "@time{%Y-%m-%d %H:%M}"), 
-                         ('Temperature', "@tl{f0.00} °C")]#
+    hover_p1.tooltips = [("Timestamp", "@time{%Y-%m-%d %H:%M}"),
+                         ('Temperature', "@tl{f0.0} °C")]#
     # sunshine duration
     if 'so' in df.columns:
         p1.extra_y_ranges = {'ssd': Range1d(start=0, end=10)}
         p1.add_layout(LinearAxis(y_range_name='ssd'), 'right')
-        p1.vbar(top='so', x='time', source=df, width=get_width(), fill_color=socol, 
+        p1.vbar(top='so', x='time', source=df, width=get_width(), fill_color=socol,
                 line_alpha=0, line_width=0, fill_alpha=0.5, y_range_name='ssd', legend = 'Sunshine duration')
         p1.yaxis[1].axis_label = 'Sunshine duration (min)'
         p1.yaxis[1].axis_label_text_font_size = font_size_label
         p1.yaxis[1].major_label_text_font_size = font_size_ticker
         p1.yaxis[1].major_label_text_color = socol
         hover_p1[0].tooltips.append(('Sunshine duration', '@so{int} min per 10 min'))
-    
+
     # temperature
     h_line = p1.line(x='time', y='tl', source=df, line_width=4, color=tcol, legend='Temperature');
     p1.yaxis[0].axis_label = 'Temperature (°C)'
-    
+
     # dew point
     if 'tp' in df.columns:
         p1.y_range=Range1d(df['tp'].min()-2, df['tl'].max()+2)
         p1.line(x='time', y='tp', source=df, line_width=4, color=hcol, legend = 'Dewpoint')
-        hover_p1[0].tooltips.append(('Dewpoint', '@tp{f0.00} °C'))
+        hover_p1[0].tooltips.append(('Dewpoint', '@tp{f0.0} °C'))
     else:
         # relative humidity
         p1.y_range=Range1d(df['tl'].min()-2, df['tl'].max()+2)
@@ -106,33 +135,33 @@ def upper_plot(df):
         p1.yaxis[1].axis_label_text_font_size = font_size_label
         p1.yaxis[1].major_label_text_font_size = font_size_ticker
         p1.yaxis[1].major_label_text_color = hcol
-        hover_p1[0].tooltips.append(('relative humidity', '@rf{f0.00} %'))
-    
+        hover_p1[0].tooltips.append(('relative humidity', '@rf{f0.0} %'))
+
     # precipitation (daily accumulated)
     if 'rrcum' in df.columns:
         if df['rrcum'].sum() > 0: #axis would disappear when there was no rain measured
             p1.extra_y_ranges['rrcum'] = Range1d(start=0, end=(df['rrcum'].max() + df['rrcum'].max()*0.1))
-        else: 
+        else:
             p1.extra_y_ranges['rrcum'] = Range1d(start=0, end=10)
         p1.add_layout(LinearAxis(y_range_name='rrcum'), 'right')
         p1.line(x='time', y='rrcum', source=df, line_width=4, color=pcol, y_range_name='rrcum', legend = 'Precipitation')
-        hover_p1[0].tooltips.append(('Cumulated rainsum', '@rrcum{f0.00} mm'))
+        hover_p1[0].tooltips.append(('Cumulated rainsum', '@rrcum{f0.0} mm'))
         p1.yaxis[2].axis_label_text_font_size = font_size_label
         p1.yaxis[2].major_label_text_font_size = font_size_ticker
         p1.yaxis[2].major_label_text_color = pcol
         p1.yaxis[2].axis_label = 'Precipitation (mm)'
         # plot rainrate but hide it by default
-        rr = p1.vbar(top='rr', x='time', source=df, width=get_width(), 
-                     fill_color='blue', line_alpha=0, 
-                     line_width=0, fill_alpha=0.5, 
+        rr = p1.vbar(top='rr', x='time', source=df, width=get_width(),
+                     fill_color='blue', line_alpha=0,
+                     line_width=0, fill_alpha=0.5,
                      legend = 'Rain rate',  y_range_name='rrcum')
         rr.visible = False
-    
+
     # hover
     hover_p1.formatters = { "time": "datetime"}
     hover_p1.mode = 'vline'
     hover_p1.renderers =[h_line] #### to fix if missing value
-    
+
     # legend
     p1.legend.location = "top_left"
     p1.legend.click_policy="hide"
@@ -143,28 +172,28 @@ def upper_plot(df):
 ##### Plot 2
 def lower_plot(df, p1):
     p2_tools = 'box_zoom,pan,save,hover,reset,xwheel_zoom'
-    p2 = figure(width = fwidth, height = fhgt,x_axis_type="datetime", 
+    p2 = figure(width = fwidth, height = fhgt,x_axis_type="datetime",
                 tools=p2_tools, x_range=p1.x_range);
-    
+
     p2 = set_font_sizes_axis(p2)
-    
+
     # pressure
     h_line = p2.line(x='time', y='p', source=df, line_width=4, color='blue', legend = 'Pressure')
-    p2.y_range=Range1d(df['p'].min()-10, df['p'].max()+10)
+    p2.y_range=Range1d(df['p'].min()-5, df['p'].max()+5)
     p2.yaxis.axis_label = 'Pressure (hPa)'
-    
+
     # wind
-    p2.extra_y_ranges = {"winddir": Range1d(start=0, end=360), 
+    p2.extra_y_ranges = {"winddir": Range1d(start=0, end=360),
                          "windspd": Range1d(start=0, end=df['ff'].max()+df['ff'].max()*0.1)}
-    
+
     p2.add_layout(LinearAxis(y_range_name='winddir'), 'right')
     p2.add_layout(LinearAxis(y_range_name="windspd"), 'right')
     p2.circle(x='time', y='dd', source=df, line_width=4, color='black', y_range_name='winddir', legend = 'Wind Direction')
     p2.line(x='time', y='ff', source=df, line_width=2, color='red', y_range_name='windspd', legend = 'Wind speed')
-   
-    
+
+
     p2.yaxis[0].axis_label = 'Pressure (hPa)'
-    p2.yaxis[1].axis_label = 'Wind direction (deg)'
+    p2.yaxis[1].axis_label = 'Wind direction (°)'
     p2.yaxis[2].axis_label = 'Wind speed (ms⁻¹)'
     p2.yaxis[1].axis_label_text_font_size = font_size_label
     p2.yaxis[1].major_label_text_font_size = font_size_ticker
@@ -172,17 +201,17 @@ def lower_plot(df, p1):
     p2.yaxis[2].major_label_text_font_size = font_size_ticker
     p2.yaxis[2].major_label_text_color = ffcol
     p2.yaxis[0].major_label_text_color = pcol
-    
+
     # hover
     hover_p2 = p2.select(dict(type=HoverTool))
-    hover_p2.tooltips = [("Timestamp", "@time{%Y-%m-%d %H:%M}"), 
-                         ('Pressure', '@p'), 
-                         ('Winddirection', '@dd'),
-                         ('Windspeed', '@ff')]
+    hover_p2.tooltips = [("Timestamp", "@time{%Y-%m-%d %H:%M}"),
+                         ('Pressure', '@p{f0.0} hPa'),
+                         ('Winddirection', '@dd{int} °'),
+                         ('Windspeed', '@ff{f0.0} (ms⁻¹)')]
     hover_p2.formatters = { "time": "datetime"}
     hover_p2.mode = 'vline'
     hover_p2.renderers =[h_line] #### to fix if missing value
-    
+
     # legend
     p2.legend.location = "top_left"
     p2.legend.click_policy="hide"
@@ -191,7 +220,7 @@ def lower_plot(df, p1):
 
 
 
-#### Setting Up 
+#### Setting Up
 output_file("acinn_weather_plot.html")
 base_url = 'http://meteo145.uibk.ac.at/'
 time = str(7)
@@ -208,12 +237,14 @@ tcol = 'red'
 socol = 'yellow'
 #447.167300, 11.457867
 # If station selection changes, change this dataframe
-stations = pd.DataFrame({'lat':[47.263631, 47.011203, 46.867521, 47.167300], 
-                         'lon':[11.385571, 11.480401, 11.024800, 11.457867]}, 
+stations = pd.DataFrame({'lat':[47.263631, 47.011203, 46.867521, 47.167300],
+                         'lon':[11.385571, 11.480401, 11.024800, 11.457867],
+                         'height':[609, 2107, 1938, 1080],
+                         },
                          index=['innsbruck', 'sattelberg', 'obergurgl', 'ellboegen']) # todo correct coordinates
 
 # filling url column
-stations['url'] = '' 
+stations['url'] = ''
 for station in stations.index:
     url = base_url + station + '/' + time
     stations['url'].loc[stations.index == station] = url
@@ -225,28 +256,40 @@ for station in stations.index:
 #### Mapplot
 tile_provider = get_provider(Vendors.CARTODBPOSITRON)
 map_tools = 'box_zoom,pan,save,hover,reset,wheel_zoom'
-map_plot = figure(x_range=(1108137, 1417582), y_range=(5895123 , 6088551), 
+map_plot = figure(x_range=(1108137, 1417582), y_range=(5895123 , 6088551),
                   plot_width=600, plot_height=350,
-                  x_axis_type="mercator", y_axis_type="mercator", 
+                  x_axis_type="mercator", y_axis_type="mercator",
                   tools=map_tools)
 map_plot.add_tile(tile_provider)
-map_plot.circle(x="x", y="y", size=15, fill_color="blue", 
+map_plot.circle(x="x", y="y", size=15, fill_color="blue",
                 fill_alpha=0.5, source=stations);
 hover_map = map_plot.select(dict(type=HoverTool))
-hover_map.tooltips = [("Station", "@index".capitalize())] # todo capitalize!!!!
+hover_map.tooltips = [("Station", "@index".capitalize()), # todo capitalize!!!!
+                      ('Height', '@height m')]
 hover_map.mode = 'mouse'
+map_plot.yaxis[0].axis_label = 'Latitude (°)'
+map_plot.xaxis[0].axis_label = 'Longitude (°)'
+map_plot.yaxis[0].axis_label_text_font_size = font_size_label
+map_plot.yaxis[0].major_label_text_font_size = font_size_ticker
+map_plot.xaxis[0].axis_label_text_font_size = font_size_label
+map_plot.xaxis[0].major_label_text_font_size = font_size_ticker
+
 
 
 
 ####### generating plots for the stations
 p1 = {}
 p2 = {}
+sts = {}
 tab = []
 for station in stations.index:
-    df = read_data(stations['url'].loc[stations.index == station]) 
+    df = read_data(stations['url'].loc[stations.index == station])
+    stats = get_stats(df).round(decimals=1)
     p1[station] = upper_plot(df)
     p2[station] = lower_plot(df, p1[station])
-    tab.append(Panel(child=column(p1[station], p2[station]), title=station.capitalize()))
+    sts[station] = Div(text="{}".format(stats.to_html()))
+    tab.append(Panel(child=column(p1[station], p2[station], sts[station]),
+                     title=station.capitalize()))
 
 
 #### Layout and save
